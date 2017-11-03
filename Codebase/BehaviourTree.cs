946e51224d13048cc23c5e1db573d1a277816141
Owen Jackson <owen2.jackson@live.uwe.ac.uk>
2017-11-03 13:58:00 +0000

    Added beginnings of parallel task types
    Need to use threading for this

--- Codebase/BehaviourTree.cs
+++ Codebase/BehaviourTree.cs
@@ -13,10 +13,10 @@ namespace BT_and_RL
     {
         enum StatusValue
         {
-            SUCCESS = 0,
-            FAILED = 1,
-            RUNNING = 2,
-            ERROR = 3
+            NULL = 0,
+            SUCCESS,
+            FAILED,
+            RUNNING,
         }
 
         //Base class for any task within a Behaviour Tree
@@ -30,6 +30,11 @@ namespace BT_and_RL
                 status = StatusValue.RUNNING;
                 return status;
             }
+
+            virtual public void Terminate()
+            {
+                status = StatusValue.FAILED;
+            }
         }
 
         //Base class for a condition check
@@ -39,14 +44,14 @@ namespace BT_and_RL
         }
 
         //Base class for a single action to perform (leaf node)
-        abstract class BTAction
+        abstract class BTAction : BTTask
         {
             private StatusValue status;
 
-            abstract public bool PerformAction();
+            abstract public StatusValue PerformAction();
         }
 
-        //Stops at first successful action
+        //A composite task that stops at first successful action
         class BTSelector : BTTask
         {
             public override StatusValue Run()
@@ -84,7 +89,7 @@ namespace BT_and_RL
             }
         }
 
-        //Stops at first failed action
+        //A composite task that stops at first failed action
         class BTSequence : BTTask
         {
             public override StatusValue Run()
@@ -121,15 +126,71 @@ namespace BT_and_RL
             }
         }
 
-        //Decorator node - only has one child
+        //A parallel task that runs its children concurrently
+        class BTParallel : BTTask
+        {
+            //List of children currently running
+            protected List<BTTask> running_children;
+
+            StatusValue result;
+
+            public override StatusValue Run()
+            {
+                result = StatusValue.NULL;
+
+                foreach(BTTask c in children)
+                {
+                    Thread thread = new Thread(() => RunChild(c));
+                    thread.Start();
+                }
+                //Sleep this thread between checks for completion
+                while(result == StatusValue.NULL)
+                {
+                    Thread.Sleep(100);
+                }
+                return result;
+            }
+
+            //Runs the current child in its own thread
+            protected void RunChild(BTTask child)
+            {
+                running_children.Add(child);
+                StatusValue returned = child.Run();
+                running_children.Remove(child);
+
+                //If the child fails, terminate
+                if(returned == StatusValue.FAILED)
+                {
+                    Terminate();
+                    result = StatusValue.FAILED;
+                }
+                //If all children succeed, this has succeeded
+                else if(running_children.Count == 0)
+                {
+                    result = StatusValue.SUCCESS;
+                }
+            }
+
+            //Parallel tasks fail when any child fails. It must then tell all other children to terminate
+            public override void Terminate()
+            {
+                foreach(BTTask c in running_children)
+                {
+                    c.Terminate();
+                }
+            }
+        }
+
+        //A decorator task that only has one child
         class BTDecorator : BTTask
         {
+            protected BTTask child = null;
             public override StatusValue Run()
             {
                 status = StatusValue.RUNNING;
-                if(children.Count > 0)
+                if(child != null)
                 {
-                    if(children[0].Run() == StatusValue.SUCCESS)
+                    if(child.Run() == StatusValue.SUCCESS)
                     {
                         status = StatusValue.SUCCESS;
                         return status;
@@ -142,12 +203,26 @@ namespace BT_and_RL
         //Decorator that inverts the its child's return value
         class BTInverter : BTDecorator
         {
-
+            public override StatusValue Run()
+            {
+                status = StatusValue.RUNNING;
+                status = base.Run();
+                //Invert the result of the child node
+                if(status == StatusValue.SUCCESS)
+                {
+                    status = StatusValue.FAILED;
+                    return status;
+                }
+                else if(status == StatusValue.FAILED)
+                {
+                    status = StatusValue.SUCCESS;
+                    return status;
+                }
+                return status;
+            }
         }
-
-
-        //Decorator node that guards a thread INCOMPLETE
-        /*
+        
+        //Decorator node that guards a thread
         class BTSemaphoreGuard : BTDecorator
         {
             private Semaphore semaphore;
@@ -159,9 +234,14 @@ namespace BT_and_RL
 
             public override StatusValue Run()
             {
-                if(semaphore.)
+                if(semaphore.WaitOne())
+                {
+                    status = child.Run();
+                    semaphore.Release();
+                    return status;
+                }
+                return StatusValue.FAILED;
             }
-        }
-        */
+        }        
     }
 }
