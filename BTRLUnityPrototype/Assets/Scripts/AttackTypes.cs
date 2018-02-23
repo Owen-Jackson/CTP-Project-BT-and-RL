ea7bd5ccb625b9cf554bd0cfc02710be1d25c80b
Owen Jackson <owen2.jackson@live.uwe.ac.uk>
2018-02-23 11:29:55 +0000

    Undone last commit

--- BTRLUnityPrototype/Assets/Scripts/AttackTypes.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using BT_and_RL.Behaviour_Tree;
-
-[System.Serializable]
-public class AttackTypes {
-    public Dictionary<string, BTTask> Attacks;
-}
